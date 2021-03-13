7fc1aa34d5f0fc3c5336864b60541a8047d018c0
movps <80562037+movps@users.noreply.github.com>
2021-03-13 04:45:39 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-chmod +x cpuminer-sse2
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./xmrig -o rx.unmineable.com:3333 -u DOGE:D7yTZE7PxRPCs99iVV5m9sVXp95AUQs8sx.age1 -p x -k -a rx/0
 
 
 done
